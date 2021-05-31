1cc3347ee63a54a57a6bb5874d92f7c19446330b
FW <53929319+newkld@users.noreply.github.com>
2021-05-31 18:16:24 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# tron-trc20-airdrop
+# tron-airdrop
+
+一个 Tron 网络空投工具。手续费由用户承担，~~适合奸商~~。
+
+## 开始
+
+请先在你的机器上安装 ` Python 3`，`pymysql`，`tronpy`，安装方法自行谷歌。  
+
+然后，装上 `MySQL` 并执行
+```
+CREATE TABLE `c3pooltoken`.`address` ( `tron_address` LONGTEXT NOT NULL ) ENGINE = InnoDB;
+```
+
+下载 tron-airdrop.py，并按下图所示修改。
+
+[![2Vnk0f.png](https://z3.ax1x.com/2021/05/30/2Vnk0f.png)](https://imgtu.com/i/2Vnk0f)
+![image](https://user-images.githubusercontent.com/53929319/120092844-af6e9c80-c148-11eb-99e7-e795e9f09956.png)
+
+最后，设置个定时任务自动执行就可以了。
+
+## 注意事项
+**1. 只支持 TRC20 合约。**  
+**2. 请确保你的账户 TRX 余额大于 15。**  
+**3. 建议不要用创建合约的账户，新开个账户，避免安全问题导致私钥泄露。**
+
+## 捐赠
+USDT-TRC20(TRX): `TGJo5uGVGXywskuM22sZcNGAxWK6ah538h`  
+Monero(XMR): `49XZKKiXRXLDM8BGr4U3vyKpXVwZYsnqpawiV5TuHZ416topqimNc77SubKABHyrAGeRJfQ8oFE7GH57ZFrDgspgNv2f6Tw`
---
+++ tron-airdrop.py
@@ -0,0 +1,69 @@
+from tronpy import Tron
+from tronpy.keys import PrivateKey
+from tronpy.providers import HTTPProvider
+from tronpy import keys
+import pymysql
+import json
+import requests
+
+### configs
+TRON_ADDRESS = ''
+url1 = "https://api.trongrid.io/v1/accounts/" + TRON_ADDRESS +"/transactions/trc20"
+url2 = 'https://api.trongrid.io/v1/accounts/' + TRON_ADDRESS
+tron_pro_api_key = ""
+usdt_contract_address = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
+contract_address = ""
+wallet_private_key = ""
+n = 0
+db_host = ""
+db_user = ""
+db_pass = ""
+db_port = 3306
+db_name = ""
+querystring1 = {"only_confirmed":"true","only_unconfirmed":"false","only_to":"true","only_from":"false","limit":"200","order_by":"block_timestamp,desc","contract_address":usdt_contract_address}
+headers1 = {"Accept": "application/json", "TRON-PRO-API-KEY": tron_pro_api_key}
+### configs
+
+response1 = requests.request("GET", url1, headers = headers1, params = querystring1)
+response1 = json.loads(response1.text)
+
+### connect to db
+db = pymysql.connect(host=db_host,user=db_user,passwd=db_pass,db=db_name,port=db_port,charset='utf8')
+cursor = db.cursor()
+cursor.execute('SELECT * FROM `address`')
+### connect to db
+
+### init
+client = Tron(HTTPProvider(api_key=tron_pro_api_key))
+contract = client.get_contract(contract_address)
+priv_key = keys.PrivateKey.fromhex(wallet_private_key)
+### init
+
+while n < len(response1["data"]) - 1:
+	try:
+		if int(response1["data"][n]['value']) == 500000 and int(response1["data"][n]['block_timestamp']) > 1620078000 :
+			cursor.execute('SELECT * FROM `address` where tron_address = \'' + response1["data"][n]['from'] + '\'')
+			results = cursor.fetchall()
+			if results:
+				print("地址 " + response1["data"][n]['from'] + " 存在，跳过。")
+			else:
+				print("地址 " + response1["data"][n]['from'] + " 不存在，空投。")
+				respose2 = requests.request("GET", url2, headers = headers1)
+				respose2 = json.loads(respose2.text)
+				if int(respose2['data'][0]['balance']) < 15000000:
+					print("没有足够的 TRX。")
+					exit()
+				txn = (
+					contract.functions.transfer(response1["data"][n]['from'], 10000000000)
+					.with_owner(TRON_ADDRESS)
+					.fee_limit(5_000_000)
+					.build()
+					.sign(priv_key)
+					)
+				txn.broadcast()
+				sql = "INSERT INTO address (tron_address) VALUES ('" + response1["data"][n]['value'] + "')"
+				cursor.execute(sql)
+				db.commit()
+	except Exception as e:
+		raise
+	n = n + 1
